41026c7d55300653044be129c4367954fea6f35b
Myethishwar <yathishwerchintu@gmail.com>
2025-04-05 08:43:52 +0530

    Added Dashboard files

--- SALES-INSIGHTS-PROJECT/Basic Analysis using SQL.sql
+++ SALES-INSIGHTS-PROJECT/Basic Analysis using SQL.sql
@@ -1,3 +1,4 @@
+use sales;
 -- Count of total customers  - 38
 SELECT COUNT(*) FROM customers;
 
@@ -35,3 +36,44 @@ FROM
     date ON transactions.order_date = date.date
 GROUP BY year ORDER BY average_sales Desc;
 
+
+-- Top customers based on sales amount
+SELECT 
+    custmer_name, SUM(sales_amount) AS total_sales_amount
+FROM
+    transactions
+        INNER JOIN
+    customers ON transactions.customer_code = customers.customer_code
+GROUP BY custmer_name ORDER BY total_sales_amount DESC LIMIT 5;
+
+-- AVERAGE Profict margin 
+SELECT ROUND(AVG(profit_margin_percentage), 2) AS avg_profit_margin FROM transactions;
+
+-- Top 5 best selling products by sales quantity 
+SELECT product_code, sales_qty FROM transactions ORDER BY sales_qty DESC LIMIT 5;
+
+-- Average sales per transaction
+SELECT ROUND(AVG(sales_amount), 2) FROM transactions;
+
+-- Profit margin by product 
+SELECT 
+    product_code, 
+    ROUND(AVG(profit_margin_percentage) * 100, 2) AS avg_profit_margin_percent
+FROM 
+    transactions
+GROUP BY product_code
+ORDER BY avg_profit_margin_percent DESC
+LIMIT 10;
+
+
+-- Revenue by Product category
+SELECT 
+    p.product_type,
+    ROUND(SUM(t.sales_amount), 0) AS total_revenue
+FROM 
+    transactions t
+    JOIN products p ON p.product_code = t.product_code
+GROUP BY p.product_type
+ORDER BY total_revenue DESC;
+
+select distinct(currency) from transactions;
